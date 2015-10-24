e1857291677df7bc11d6c84be942eca39a93736f
Juan Pablo Marzetti <yonpols@groupon.com>
2015-10-24 10:46:04 -0700

    Initial Commit

---
+++ week1-assignment-rt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  week1-assignment-rt
+//
+//  Created by Juan Pablo Marzetti on 10/24/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ week1-assignment-rt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  week1-assignment-rt
+//
+//  Created by Juan Pablo Marzetti on 10/24/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
